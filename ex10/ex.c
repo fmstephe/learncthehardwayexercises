e6d2d954a9624d60938b10932c8f17fcad453a9e
Francis <francis@ubuntu.(none)>
2013-04-15 12:33:35 +0100

    Exercise 10

--- ex10/ex.c
+++ ex10/ex.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
+	int i = 0;
+	// Go through each string in argv
+	// NB: argv[0] contains the name of the application being run
+	for (i = 0; i < argc; i++) {
+		printf("arg %d: %s\n", i, argv[i]);
+	}
+
+	// Let's make our own array of strings
+	char *states[] = { "California", "Oregon", "Washington", "Texas" };
+	int num_states = 4;
+
+	for (i = 0; i < num_states; i++) {
+		printf("State %d: %s\n", i, states[i]);
+	}
+
+	return 0;
 }
